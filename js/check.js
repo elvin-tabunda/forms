660f25d72437c1fa787fb2d7951e548b925ef8a7
Elvin <tabundaelvin@gmail.com>
2024-04-11 13:14:29 +0800

    Update check.js

--- js/check.js
+++ js/check.js
@@ -108,7 +108,7 @@ function selectForms() {
             window.location.href = '../forms/checkVoucher.html';
             break;
         case '2':
-            window.location.href = '../forms.html';
+            window.location.href = '../index.html';
             break;
         case '3':
             window.location.href = '../forms/budgetRequestForm.html';
